62a03e4bbe315f872f16330bc399688b51835d5a
Julian <julianstamb@gmail.com>
2023-12-26 04:56:46 +0100

    first commit

---
+++ Meshoptimizer.NET/src/Meshopt.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Meshoptimizer
+{
+    public static unsafe partial class Meshopt
+    {
+        private const string LIBRARY_NAME = "meshoptimizer";
+
+        public struct Meshlet
+        {
+            /* offsets within meshlet_vertices and meshlet_triangles arrays with meshlet data */
+            public uint VertexOffset;
+            public uint TriangleOffset;
+
+            /* number of vertices and triangles used in the meshlet; data is stored in consecutive range defined by offset and count */
+            public uint VertexCount;
+            public uint TriangleCount;
+        };
+
+        public struct Stream
+        {
+            public void* Data;
+            public nuint Size;
+            public nuint Stride;
+        };
+
+        [LibraryImport(LIBRARY_NAME, EntryPoint = "meshopt_buildMeshlets")]
+        public static partial nuint BuildMeshlets(ref Meshlet meshlets,
+            in uint meshletVertices,
+            in byte meshletTriangles,
+            in uint indices,
+            nuint indexCount,
+            in float vertexPositions,
+            nuint vertexCount,
+            nuint vertexPositionsStride,
+            nuint maxVertices,
+            nuint maxTriangles,
+            float coneWeight);
+
+
+        [LibraryImport(LIBRARY_NAME, EntryPoint = "meshopt_buildMeshletsBound")]
+        public static partial nuint BuildMeshletsBound(nuint indexCount, nuint maxVertices, nuint maxTriangles);
+
+        [LibraryImport(LIBRARY_NAME, EntryPoint = "meshopt_generateVertexRemapMulti")]
+        public static partial nuint GenerateVertexRemapMulti(ref uint destination, in uint indices, nuint index_count, nuint vertex_count, in Stream streams, nuint stream_count);
+
+        [LibraryImport(LIBRARY_NAME, EntryPoint = "meshopt_remapIndexBuffer")]
+        public static partial void RemapIndexBuffer(ref uint destination, in uint indices, nuint index_count, in uint remap);
+
+        [LibraryImport(LIBRARY_NAME, EntryPoint = "meshopt_remapVertexBuffer")]
+        public static partial void RemapVertexBuffer(void* destination, void* vertices, nuint vertex_count, nuint vertex_size, in uint remap);
+
+        [LibraryImport(LIBRARY_NAME, EntryPoint = "meshopt_optimizeVertexCache")]
+        public static partial void OptimizeVertexCache(ref uint destination, in uint indices, nuint index_count, nuint vertex_count);
+
+        [LibraryImport(LIBRARY_NAME, EntryPoint = "meshopt_optimizeVertexFetchRemap")]
+        public static partial nuint OptimizeVertexFetchRemap(ref uint destination, in uint indices, nuint index_count, nuint vertex_count);
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Meshoptimizer.NET
+
+C# bindings for [meshoptimizer](https://github.com/zeux/niagara/tree/5c4da32850ae32a0528f11e22e09ada5348f4e0c).
+
+Available as NuGet package under [Meshoptimizer.NET](https://www.nuget.org/packages/Meshoptimizer.NET/1.0.0).
+
